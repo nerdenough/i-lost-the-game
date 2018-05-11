5c0ce285692c5bbbf1d055a0a436912a386af4cd
Brendan Goodenough <brendan@goodenough.nz>
2018-05-12 08:51:53 +1200

    Initial Commit

---
+++ ILostTheGame/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ILostTheGame
+//
+//  Created by Brendan Goodenough on 12/05/18.
+//  Copyright © 2018 Brendan Goodenough. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
